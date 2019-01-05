5d10499a88f2cabd8b2a2c37189438be734c5474
hiproz@gmail.com <hiproz@gmail.com>
2019-01-05 16:13:04 +0800

    reduce the variaty of file name and line num

--- clog.c
+++ clog.c
@@ -2,34 +2,14 @@
 #include <stdarg.h>
 #include "clog.h"
 
-////////////////////////depend on the platform////////////////////////////
-// need the user to impletation this function,it is variaty that every platform get the rtc time。
-// 需要业务自己实现这个函数,每个平台获取时间的实现都有所不同。
-// datetime :为长度为6的字节数组，返回年月日时分秒
-extern void clog_get_datetime(unsigned char *datetime);
-//////////////////////////////////////////////////////////////////////////
-
 int clog_level = INF;
-char clog_file_name[CLOG_MAX_FILE_NAME_LEN];
-int clog_line_num = 0;
 char clog_buf[CLOG_BUF_SIZE] = {0};
 
 void _clog(int log_level, char *fmt, ...) {
-
   if (fmt == 0 || strlen(fmt) == 0)
     return;
 
   if (log_level >= clog_level) {
-    memset(clog_buf, 0, sizeof(clog_buf));
-
-    // date time and code line number.
-    unsigned char datetime[6] = {0};
-    clog_get_datetime(datetime);
-
-    snprintf(clog_buf + strlen(clog_buf), sizeof(clog_buf) - 1 - strlen(clog_buf), "%02u/%02u/%02u %02u:%02u:%02u %s:%d ",
-             datetime[0], datetime[1], datetime[2], datetime[3], datetime[4], datetime[5],
-             clog_file_name, clog_line_num);
-
     // log level filter
     if (log_level == INF) {
       strcat(clog_buf, "[INF] ");
@@ -47,7 +27,6 @@ void _clog(int log_level, char *fmt, ...) {
     }
 
     va_list args;
-
     va_start(args, fmt);
     vsnprintf(clog_buf + strlen(clog_buf), sizeof(clog_buf) - 1 - strlen(clog_buf), fmt, args);
     va_end(args);
--- clog.h
+++ clog.h
@@ -10,23 +10,27 @@ extern "C" {
 #include "SEGGER_RTT.h"
 
 #define printf(fmt, ...) SEGGER_RTT_printf(0, fmt, ##__VA_ARGS__)
-////////////////////////////////////////////////////////////////////
-#define CLOG_MAX_FILE_NAME_LEN 64
-#define CLOG_BUF_SIZE 768
 
-extern char clog_file_name[CLOG_MAX_FILE_NAME_LEN];
-extern int clog_line_num;
+// it is variaty that every platform get the rtc time。
+// 每个平台获取时间的实现都有所不同。
+// datetime :为长度为6的字节数组，返回年月日时分秒
+extern void clog_get_datetime(unsigned char *datetime);
+////////////////////////////////////////////////////////////////////
+
+#define CLOG_BUF_SIZE 768
+extern char clog_buf[CLOG_BUF_SIZE];
 
 // clang-format off
 #define clog { \
-    memset(clog_file_name, 0, sizeof(clog_file_name));  \
+    unsigned char datetime[6] = {0}; \
+    clog_get_datetime(datetime); \
+    memset(clog_buf, 0, sizeof(clog_buf)); \
     char *p = strrchr(__FILE__, '\\');  \
     if (p != NULL) { \
-      strncpy(clog_file_name, strrchr(__FILE__, '\\') + 1, CLOG_MAX_FILE_NAME_LEN-1); \
+      snprintf(clog_buf, CLOG_BUF_SIZE - 1, "%02u/%02u/%02u %02u:%02u:%02u %s:%d ", datetime[0], datetime[1], datetime[2], datetime[3], datetime[4], datetime[5], strrchr(__FILE__, '\\') + 1, __LINE__); \
     } else { \
-			strncpy(clog_file_name, strrchr(__FILE__, '/') + 1, CLOG_MAX_FILE_NAME_LEN-1); \
+      snprintf(clog_buf, CLOG_BUF_SIZE - 1, "%02u/%02u/%02u %02u:%02u:%02u %s:%d ", datetime[0], datetime[1], datetime[2], datetime[3], datetime[4], datetime[5], strrchr(__FILE__, '/') + 1, __LINE__); \
     } \
-		clog_line_num = __LINE__; \
   } _clog
 // clamg-format on
 
