e6f7bf923c4542ad0838b102db2a67997acf69ed
hiproz@gmail.com <hiproz@gmail.com>
2019-01-05 15:41:14 +0800

    adapte to rtt

--- clog.c
+++ clog.c
@@ -1,48 +1,67 @@
-
-// clang-format off
+#include "stdio.h"
 #include <stdarg.h>
 #include "clog.h"
-// clang-format on
 
-char clog_buf[CLOG_BUF_SIZE] = {0};
+////////////////////////depend on the platform////////////////////////////
+// need the user to impletation this function,it is variaty that every platform get the rtc time。
+// 需要业务自己实现这个函数,每个平台获取时间的实现都有所不同。
+// datetime :为长度为6的字节数组，返回年月日时分秒
+extern void clog_get_datetime(unsigned char *datetime);
+//////////////////////////////////////////////////////////////////////////
+
 int clog_level = INF;
+char clog_file_name[CLOG_MAX_FILE_NAME_LEN];
+int clog_line_num = 0;
+char clog_buf[CLOG_BUF_SIZE] = {0};
 
-/*****************************************************************************
-
- *****************************************************************************/
 void _clog(int log_level, char *fmt, ...) {
-  va_list args;
-  char buf[CLOG_BUF_SIZE] = {0};
 
-  if (fmt == NULL || strlen(fmt) == 0)
+  if (fmt == 0 || strlen(fmt) == 0)
     return;
 
   if (log_level >= clog_level) {
-    // 日志级别
+    memset(clog_buf, 0, sizeof(clog_buf));
+
+    // date time and code line number.
+    unsigned char datetime[6] = {0};
+    clog_get_datetime(datetime);
+
+    snprintf(clog_buf + strlen(clog_buf), sizeof(clog_buf) - 1 - strlen(clog_buf), "%02u/%02u/%02u %02u:%02u:%02u %s:%d ",
+             datetime[0], datetime[1], datetime[2], datetime[3], datetime[4], datetime[5],
+             clog_file_name, clog_line_num);
+
+    // log level filter
     if (log_level == INF) {
       strcat(clog_buf, "[INF] ");
-    }
-    if (log_level == RUN) {
-      strcat(clog_buf, "[RUN] ");
-    }
-    if (log_level == WAR) {
+    } else if (log_level == DBG) {
+      strcat(clog_buf, "[DBG] ");
+    } else if (log_level == WAR) {
       strcat(clog_buf, "[WAR] ");
-    }
-    if (log_level == ERR) {
+    } else if (log_level == RUN) {
+      strcat(clog_buf, "[RUN] ");
+    } else if (log_level == ERR) {
       strcat(clog_buf, "[ERR] ");
+    } else {
+      //do nothing
+      return;
     }
 
-    // 打印的内容
-    va_start(args, fmt);
-    vsnprintf(buf, CLOG_BUF_SIZE, fmt, args);
-    va_end(args);
-    strncat(clog_buf, buf, CLOG_BUF_SIZE);
+    va_list args;
 
-    // 如果原始内容，没有加回车换行，这里统一增加回车换行
-    // strncat(clog_buf, "\r\n", 2);
-    // 打印输出
-    printf("%s", clog_buf);
+    va_start(args, fmt);
+    vsnprintf(clog_buf + strlen(clog_buf), sizeof(clog_buf) - 1 - strlen(clog_buf), fmt, args);
+    va_end(args);
+
+    printf(clog_buf);
   }
 }
-void clog_init(void) { ; }
-void clog_set_level(int level) { clog_level = level; }
+
+// init the clog, put some initialize or resource option in here
+void clog_init(void) {
+  clog_level = INF;
+}
+
+// set the log level filter
+void clog_set_level(int level) {
+  clog_level = level;
+}
--- clog.h
+++ clog.h
@@ -1,94 +1,38 @@
 #ifndef __CLOG_H__
 #define __CLOG_H__
+#include "string.h"
+//////////////////////////////depend on the platform 需要根据不同的平台适配////////////////
+#include "SEGGER_RTT.h"
 
-// clang-format off
-
-// clang-format on
-
-#ifndef ERROR
-#define ERROR 1
-#endif
-#ifndef SUCCESS
-#define SUCCESS 0
-#endif
-#ifndef BOOL
-#define BOOL int
-#endif
-#ifndef TRUE
-#define TRUE 1
-#endif
-#ifndef FALSE
-#define FALSE 0
-#endif
-
-#ifndef NULL
-#define NULL 0
-#endif
-
-#ifndef U8
-typedef unsigned char U8;
-#endif
-#ifndef S8
-typedef signed char S8;
-#endif
-#ifndef U16
-typedef unsigned short U16;
-#endif
-#ifndef S16
-typedef signed short S16;
-#endif
-#ifndef U32
-typedef unsigned int U32;
-#endif
-#ifndef S32
-typedef signed int S32;
-#endif
-
-#define FREE_RTOS 1
-
-#if FREE_RTOS
-#define s ((xTaskGetTickCount() / configTICK_RATE_HZ))
-#define CLOG_YEAR 1
-#define CLOG_MONTH 1
-#define CLOG_DAY 1
-#define CLOG_WEEK 1
-#define CLOG_HOUR ((s % (3600 * 24)) / 3600)
-#define CLOG_MINUTE ((s % (3600)) / 60)
-#define CLOG_SECOND (s % 60)
-#endif
-
-// adapting the snprintf method
-#if APOLLO_SDK
-#define snprintf _snprintf
-#endif
+#define printf(fmt, ...) SEGGER_RTT_printf(0, fmt, ##__VA_ARGS__)
+////////////////////////////////////////////////////////////////////
+#define CLOG_MAX_FILE_NAME_LEN 64
 #define CLOG_BUF_SIZE 768
 
-#define clog                                                                                                           \
-  {                                                                                                                    \
-    memset(clog_buf, 0, sizeof(clog_buf));                                                                             \
-    char *p = strrchr(__FILE__, '\\');                                                                                 \
-    if (p != NULL) {                                                                                                   \
-      snprintf(clog_buf, sizeof(clog_buf), "%02u/%02u/%02u %02u:%02u:%02u %s:%d ", CLOG_YEAR, CLOG_MONTH, CLOG_DAY,    \
-               CLOG_HOUR, CLOG_MINUTE, CLOG_SECOND, strrchr(__FILE__, '\\') + 1, __LINE__);                            \
-    } else {                                                                                                           \
-      snprintf(clog_buf, sizeof(clog_buf), "%02u/%02u/%02u %02u:%02u:%02u %s:%d ", CLOG_YEAR, CLOG_MONTH, CLOG_DAY,    \
-               CLOG_HOUR, CLOG_MINUTE, CLOG_SECOND, strrchr(__FILE__, '/') + 1, __LINE__);                             \
-    }                                                                                                                  \
-  }                                                                                                                    \
-  _clog
-/*******************************************************
+extern char clog_file_name[CLOG_MAX_FILE_NAME_LEN];
+extern int clog_line_num;
 
- *********************************************************/
+// clang-format off
+#define clog { \
+    memset(clog_file_name, 0, sizeof(clog_file_name));  \
+    char *p = strrchr(__FILE__, '\\');  \
+    if (p != NULL) { \
+      strncpy(clog_file_name, strrchr(__FILE__, '\\') + 1, CLOG_MAX_FILE_NAME_LEN-1); \
+    } else { \
+			strncpy(clog_file_name, strrchr(__FILE__, '/') + 1, CLOG_MAX_FILE_NAME_LEN-1); \
+    } \
+		clog_line_num = __LINE__; \
+  } _clog
+// clamg-format on
+
 typedef enum {
-
   INF,
+	DBG,
   WAR,
   ERR,
   RUN,
   NONE
 } CLEVEL_EN;
-
-extern char clog_buf[CLOG_BUF_SIZE];
 
 void _clog(int log_level, char *fmt, ...);
 void clog_init(void);
